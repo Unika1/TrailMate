7433685dfa10ac9354254270b1c915d8b6632344
Unika1 <unikaprj@gmail.com>
2026-06-08 22:52:06 +0545

    app theme added

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF0F766E);
+  static const brand = Color(0xFF14B8A6); // prototype bright turquoise (buttons, headers, nav)
+  static const splashButton = Color(0xFF2DD4BF); // bright turquoise (Get Started)
+  static const fieldFill = Color(0xFFF1F4F8); // filled input background
+  static const primarySoft = Color(0xFFD9F5F0);
+  static const accent = Color(0xFF2F855A);
+  static const dark = Color(0xFF1F2937);
+  static const textGrey = Color(0xFF64748B);
+  static const mutedText = Color(0xFF94A3B8);
+  static const background = Color(0xFFF8FAFC);
+  static const backgroundAlt = Color(0xFFF1F5F9);
+  static const card = Colors.white;
+  static const danger = Color(0xFFEF4444);
+  static const border = Color(0xFFE2E8F0);
+}
