03f84002f946df1a25105fa22ba678439e019565
Unika1 <unikaprj@gmail.com>
2026-06-08 22:56:27 +0545

    api constants added

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  // Android emulator: http://10.0.2.2:5000/api
+  // Physical phone: replace with your laptop IP address.
+  static const baseUrl = 'http://10.0.2.2:5000/api';
+}
